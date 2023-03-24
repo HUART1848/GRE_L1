1234108455813e6b589d133feae43d092f84606a
Hugo Huart <hugo.huart@heig-vd.ch>
2023-03-24 11:45:19 +0100

    dsf: start

--- src/main/java/gre/lab1/groupe16/DFSMazeGenerator.java
+++ src/main/java/gre/lab1/groupe16/DFSMazeGenerator.java
@@ -8,16 +8,34 @@ import java.util.ArrayList;
 
 // TODO: javadoc
 public final class DFSMazeGenerator implements MazeGenerator {
-  public void dfs(ArrayList<Integer> u) {
+  /**
+   * Date courante.
+   */
+  private int date;
+
+  /**
+   * Tableau des dates de début de traitement.
+   */
+  private int[] start;
+
+  private void dfs(MazeBuilder builder, int u) {
 
   }
 
-  public void dfs(Graph topology) {
+  private void dfs(MazeBuilder builder) {
+    start = new int[builder.topology().nbVertices()];
+    date = 0;
 
+    for (int u = 0; u < builder.topology().nbVertices(); ++u) {
+      if (start[u] == 0) {
+        dfs(builder, u);
+      }
+    }
   }
 
   @Override
   public void generate(MazeBuilder builder, int from) {
+    ((GridGraph) builder.topology()).printGraph();
     // TODO: A implémenter
     //  NOTES D'IMPLÉMENTATION :
     //  Afin d'obtenir l'affichage adéquat, indiquer la progression (en tant que label du sommet traité) :
